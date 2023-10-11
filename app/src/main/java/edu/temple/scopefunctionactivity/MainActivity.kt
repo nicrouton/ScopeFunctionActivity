c36f8b714672839dce5779b1534d5572fb119cb0
nickc <tug61145@temple.edu>
2023-10-11 16:24:38 -0400

    Changed final function to use scope function

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -63,19 +63,18 @@ class MainActivity : AppCompatActivity() {
 
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
-
-        textView.text = collection[position].toString()
-
-        return textView
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context) = if (recycledView != null) (recycledView as TextView).apply {
+        text = collection[position].toString()
+    } else TextView(context).apply {
+        setPadding(5, 10, 10, 0)
+        textSize = 22f
+        text = collection[position].toString()
     }
+
+        // check if recycled view is null use it otherwise  create a text view and create a textView
+
+
+
+
+
 
